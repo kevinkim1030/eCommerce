9a1137f5c48d89e3f603419fc945ecf3bf5bab70
kevinkim1030 <kevin.kim1030@gmail.com>
2021-06-23 15:24:40 -0400

    user signin and signout

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require('express');
-const app = express();
-require('dotenv').config();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
 
-app.get('/', (req, res) => {
-  res.send('hello from node');
-});
+require('dotenv').config();
+// import routes
+const userRoutes = require('./routes/user')
+
+// app
+const app = express();
+
+// db
+mongoose.connect(process.env.DATABASE,{
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true 
+}).then(() => console.log('DB Connected'))
+
+mongoose.connection.on('error', err => {
+  console.log(`DB connectin error:${err.message}`)
+})
+
+// middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+
+// routes middleware
+app.use('/api', userRoutes);
 
 const PORT = process.env.PORT || 5000
 

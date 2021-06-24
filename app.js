bf377bb71b13682ef6b8e288a0883c0449782cec
kevinkim1030 <kevin.kim1030@gmail.com>
2021-06-23 22:23:21 -0400

    product queries

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors')
 const expressValidator = require('express-validator');
 
 require('dotenv').config();
@@ -34,6 +35,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 // routes middleware
 app.use('/api', authRoutes);
